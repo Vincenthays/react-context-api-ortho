143e50d08d28c7e837e140c7e6fee47b4cef59de
Vincent <vincent.hays@dataimpact.io>
2020-10-11 16:51:29 +0200

    Updates

---
+++ precache-manifest.8c32f7fe10d296e45c335057b20695a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3c295846d2050e6b9ad83dc090bc86",
+    "url": "/react-context-api-ortho/index.html"
+  },
+  {
+    "revision": "8e6252268ecd276e62f4",
+    "url": "/react-context-api-ortho/static/css/main.00909b77.chunk.css"
+  },
+  {
+    "revision": "9991b282bf2c7833ebe1",
+    "url": "/react-context-api-ortho/static/js/2.355562b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-context-api-ortho/static/js/2.355562b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6252268ecd276e62f4",
+    "url": "/react-context-api-ortho/static/js/main.ff6fa7d2.chunk.js"
+  },
+  {
+    "revision": "81ec87101c541117255a",
+    "url": "/react-context-api-ortho/static/js/runtime-main.a7427ff9.js"
+  }
+]);
