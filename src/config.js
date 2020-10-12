c8689a63236c1048c690e369878020bd71df8baf
Vincent <vincent.hays@dataimpact.io>
2020-10-12 20:56:34 +0200

    change config

--- src/config.js
+++ src/config.js
@@ -16,9 +16,14 @@ export default [
         'name': '+3 ans',
         'image': 'images/+3.jpg',
         'therapies': [
-            { 'name': 'Therapie 1', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 2', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 3', 'video': 'C04dGAkIyPM' },
+            { 'name': 'ACT', 'video': 'MfYFtUeoZH0' },
+            { 'name': 'Westmeadprogramm', 'video': 'O56YJ8Pw5YI' },
+            { 'name': 'Thérapie de groupe', 'video': 'hXzS8w6KwMQ' },
+            { 'name': 'Shapiro', 'video': '2pcVVY6BThw' },
+            { 'name': 'Parlé relax', 'video': '5OHDbAZBB6g' },
+            { 'name': 'Thérapie familale', 'video': '9tT9zGwkSrM' },
+            { 'name': 'Approche métaphorique Van Hout et Estienne', 'video': 'aM9v5CsaZPg' },
+            { 'name': 'Méditation de pleine conscience', 'video': 'JJ3JsjmXlhY' },
         ]
     }, {
         'name': '6-8 ans',
@@ -32,25 +37,40 @@ export default [
         'name': '11-14 ans',
         'image': 'images/11-14.jpg',
         'therapies': [
-            { 'name': 'Therapie 1', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 2', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 3', 'video': 'C04dGAkIyPM' },
+            { 'name': 'ACT', 'video': 'MfYFtUeoZH0' },
+            { 'name': 'Westmeadprogramm', 'video': 'O56YJ8Pw5YI' },
+            { 'name': 'Thérapie de groupe', 'video': 'hXzS8w6KwMQ' },
+            { 'name': 'Shapiro', 'video': '2pcVVY6BThw' },
+            { 'name': 'Parlé relax', 'video': '5OHDbAZBB6g' },
+            { 'name': 'Thérapie familale', 'video': '9tT9zGwkSrM' },
+            { 'name': 'Approche métaphorique Van Hout et Estienne', 'video': 'aM9v5CsaZPg' },
+            { 'name': 'Méditation de pleine conscience', 'video': 'JJ3JsjmXlhY' },
         ]
     }, {
         'name': 'Ado',
         'image': 'images/ado.jpg',
         'therapies': [
-            { 'name': 'Therapie 1', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 2', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 3', 'video': 'C04dGAkIyPM' },
+            { 'name': 'ACT', 'video': 'MfYFtUeoZH0' },
+            { 'name': 'Westmeadprogramm', 'video': 'O56YJ8Pw5YI' },
+            { 'name': 'Thérapie de groupe', 'video': 'hXzS8w6KwMQ' },
+            { 'name': 'Shapiro', 'video': '2pcVVY6BThw' },
+            { 'name': 'Parlé relax', 'video': '5OHDbAZBB6g' },
+            { 'name': 'Thérapie familale', 'video': '9tT9zGwkSrM' },
+            { 'name': 'Approche métaphorique Van Hout et Estienne', 'video': 'aM9v5CsaZPg' },
+            { 'name': 'Méditation de pleine conscience', 'video': 'JJ3JsjmXlhY' },
         ]
     }, {
         'name': 'Adulte',
         'image': 'images/adult.jpg',
         'therapies': [
-            { 'name': 'Therapie 1', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 2', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 3', 'video': 'C04dGAkIyPM' },
+            { 'name': 'ACT', 'video': 'MfYFtUeoZH0' },
+            { 'name': 'Westmeadprogramm', 'video': 'O56YJ8Pw5YI' },
+            { 'name': 'Thérapie de groupe', 'video': 'hXzS8w6KwMQ' },
+            { 'name': 'Shapiro', 'video': '2pcVVY6BThw' },
+            { 'name': 'Parlé relax', 'video': '5OHDbAZBB6g' },
+            { 'name': 'Thérapie familale', 'video': '9tT9zGwkSrM' },
+            { 'name': 'Approche métaphorique Van Hout et Estienne', 'video': 'aM9v5CsaZPg' },
+            { 'name': 'Méditation de pleine conscience', 'video': 'JJ3JsjmXlhY' },
         ]
     }
 ]
