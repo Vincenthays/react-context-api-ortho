59192952072b59c7f6ec3d42be074bff69c1c9fe
Vincent <vincent.hays@dataimpact.io>
2020-10-12 20:59:39 +0200

    update config

--- src/config.js
+++ src/config.js
@@ -29,9 +29,14 @@ export default [
         'name': '6-8 ans',
         'image': 'images/6-8.jpg',
         'therapies': [
-            { 'name': 'Therapie 1', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 2', 'video': 'C04dGAkIyPM' },
-            { 'name': 'Therapie 3', 'video': 'C04dGAkIyPM' },
+            { 'name': 'ACT', 'video': 'MfYFtUeoZH0' },
+            { 'name': 'Westmeadprogramm', 'video': 'O56YJ8Pw5YI' },
+            { 'name': 'Thérapie de groupe', 'video': 'hXzS8w6KwMQ' },
+            { 'name': 'Shapiro', 'video': '2pcVVY6BThw' },
+            { 'name': 'Parlé relax', 'video': '5OHDbAZBB6g' },
+            { 'name': 'Thérapie familale', 'video': '9tT9zGwkSrM' },
+            { 'name': 'Approche métaphorique Van Hout et Estienne', 'video': 'aM9v5CsaZPg' },
+            { 'name': 'Méditation de pleine conscience', 'video': 'JJ3JsjmXlhY' },
         ]
     }, {
         'name': '11-14 ans',
